2f07509455f92f50437a4374773ab98949d40c5f
Arun Shenoy <devarunshenoy99@gmail.com>
2019-08-07 13:35:19 +0530

    added basic socket funtionality

--- src/index.js
+++ src/index.js
@@ -11,9 +11,21 @@ const io = socketio(server)
 //PATHS
 const publicPath = app.use(express.static(path.join(__dirname,'../public')))
 
-io.on('connection',()=>{
-    console.log('New Connection')
+
+io.on('connection',(socket)=>{
+    console.log('New WebSocket')
+    socket.emit('message','Welcome')
+    socket.broadcast.emit('message','A new user has joined the chat')        //SEND TO EVERY SOCKET EXCEPT THIS ONE
+
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message)          //SEND TO EVERY SOCKET INCLUDING THIS ONE
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat')
+    })
 })
+
 
 server.listen(process.env.PORT,()=>{
     console.log('Server started on port '+process.env.PORT)
